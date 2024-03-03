e32de33389f191a647e4fed1945cc501c03935c9
Rubentym <147134353+Rubentym@users.noreply.github.com>
2024-03-03 08:39:15 +0100

    Add files via upload

---
+++ Inventory System.py
@@ -0,0 +1,7 @@
+def count_books_from_inventory(inventory):
+    return len(inventory)
+
+# Example usage:
+inventory = [{'title': 'Book 1'}, {'title': 'Book 2'}, {'title': 'Book 3'}]
+total_books = count_books_from_inventory(inventory)
+print("Total books from inventory:", total_books)
